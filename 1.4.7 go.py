3d36ff884a8ae5aad180f554f31eb29f181862be
JGardunoo <jg.garduno14@gmail.com>
2015-01-13 08:46:46 -0600

    files

--- 1.4.7 go.py
+++ 1.4.7 go.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import os.path              
 
 directory = os.path.dirname(os.path.abspath(__file__))  
-GoPro-Hero-4_file = os.path.join(directory, 'GoPro-Hero-4.jpg')
+GoPro_Hero_4_file = os.path.join(directory, 'GoPro-Hero-4.jpg')
 
-GoPro-Hero-4_img = PIL.Image.open(GoPro-Hero-4_file)
+GoPro_Hero_4_img = PIL.Image.open(GoPro_Hero_4_file)
 fig, axes = plt.subplots(1, 2)
-axes[0].imshow(GoPro-Hero-4_img, interpolation='none')
+axes[0].imshow(GoPro_Hero_4_img, interpolation='none')
