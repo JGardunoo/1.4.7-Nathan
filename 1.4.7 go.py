772333c9537f87e05c94e2df0f56e8977a7667bf
JGardunoo <jg.garduno14@gmail.com>
2015-01-14 08:44:30 -0600

    jair
    canopy

--- 1.4.7 go.py
+++ 1.4.7 go.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 import os.path              
 
 directory = os.path.dirname(os.path.abspath(__file__))  
-GoPro_Hero_4_file = os.path.join(directory, 'GoPro-Hero-4.jpg')
+GoPro_file = os.path.join(directory, 'GoPro.jpg')
 
-GoPro_Hero_4_img = PIL.Image.open(GoPro_Hero_4_file)
-fig, axes = plt.subplots(1, 2)
-axes[0].imshow(GoPro_Hero_4_img, interpolation='none')
+GoPro_img = PIL.Image.open(GoPro_file)
+fig, ax = plt.subplots(1, 1)
+ax.axis('off')
+ax.imshow(GoPro_img, interpolation='none')
+
+fig.show()
+
