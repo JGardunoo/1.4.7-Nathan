d67268876cf8bd88b77e3587ab42b3d7a6b9ea6a
JGardunoo <jg.garduno14@gmail.com>
2015-01-22 11:06:36 -0600

    Final
    Finished

--- 1.4.7 go.py
+++ 1.4.7 go.py
@@ -1,10 +1,26 @@
 import PIL
 import matplotlib.pyplot as plt
 import os.path              
-
+#open picture of GoPro camera
 directory = os.path.dirname(os.path.abspath(__file__))  
-GoPro-Hero-4_file = os.path.join(directory, 'GoPro-Hero-4.jpg')
+GoPro_file = os.path.join(directory, 'GoPro.jpg')
+#show image
+GoPro_img = PIL.Image.open(GoPro_file)
+fig, axes = plt.subplots(1, 1)
+axes.imshow(GoPro_img, interpolation='none')
+fig.show()
 
-GoPro-Hero-4_img = PIL.Image.open(GoPro-Hero-4_file)
-fig, axes = plt.subplots(1, 2)
-axes[0].imshow(GoPro-Hero-4_img, interpolation='none')
+# Open, resize, and display GoPro logo
+GoPro_logo_file = os.path.join(directory, 'GoPro_logo.jpg')
+GoPro_logo_img = PIL.Image.open(GoPro_logo_file)
+GoPro_logo_small = GoPro_logo_img.resize((160,110))
+fig2,axes2 = plt.subplots(1, 1)
+axes2.imshow(GoPro_logo_img)
+fig2.show()
+
+GoPro_img.paste(GoPro_logo_small, (360, 115))
+fig3, axes3 = plt.subplots(1, 1)
+axes3.imshow(GoPro_img, interpolation='none')
+
+axes3.axis('off')
+fig3.show()
